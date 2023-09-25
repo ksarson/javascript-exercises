433d85abb5ac0f65dca84f7d2aa54b122fc6a674
ksarson <37591778+ksarson@users.noreply.github.com>
2023-09-25 19:32:38 -0400

    Completed exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(repeatString, reps) {
+    if (reps < 0){
+        return 'ERROR';
+    }
+    let newString = '';
+    for(i=0; i<reps; i++){
+        newString+=(repeatString);
+    }
+    return newString;
 };
 
 // Do not edit below this line
