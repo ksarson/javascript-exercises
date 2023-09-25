1156f5eba4f4136a26233aea1596137d9661163d
ksarson <37591778+ksarson@users.noreply.github.com>
2023-09-25 19:40:00 -0400

    Completed exercies 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(reverseString) {
+    let charArray = reverseString.split('');
+    charArray =  charArray.reverse();
+    return charArray.join('');;
 };
 
 // Do not edit below this line
