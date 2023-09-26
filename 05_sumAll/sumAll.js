1747968fc48aca0a6c7a614ea1a11eaaddff74b7
ksarson <37591778+ksarson@users.noreply.github.com>
2023-09-25 20:32:00 -0400

    Compeleted exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    console.log(typeof num2);
+    console.log(typeof num2);
+    if(num1 < 0 || num2 < 0 || typeof(num1) !== "number" || typeof(num2) !== "number"){
+        return 'ERROR';
+    }
 
+    let sum=0;
+    let largerNum = num2 >= num1 ? num2 : num1;
+    let smallerNum = num2 < num1 ? num2 : num1;
+
+    console.log(smallerNum);
+    console.log(largerNum);
+
+    for(i=smallerNum; i<=largerNum; i++){
+        sum+=i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
