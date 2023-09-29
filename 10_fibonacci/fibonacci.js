eec820fbd1ac4b13b9546e65c289670a2fb25b1f
ksarson <37591778+ksarson@users.noreply.github.com>
2023-09-29 17:46:27 -0400

    Compeleted exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if(n < 0)
+    {
+        return "OOPS"
+    }
+    if (n < 3)
+    {
+        return 1;
+    } 
+    else 
+    {
+        return fibonacci(n-2) + fibonacci(n-1);
+    }
 };
 
 // Do not edit below this line
