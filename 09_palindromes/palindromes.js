bf3bf5a34d65c3416139a4dfd8a63b71b6e855c5
ksarson <37591778+ksarson@users.noreply.github.com>
2023-09-29 17:24:48 -0400

    Completed exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.toLowerCase().replace(/[^a-zA-Z0-9]/g,'');
+    let charArray = word.split('');
+    let reversedWord = '';
+    while (charArray.length > 0)
+    {
+        reversedWord += charArray.pop();
+    }
+    return reversedWord === word;
 };
 
 // Do not edit below this line
