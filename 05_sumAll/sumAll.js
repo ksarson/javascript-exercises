2919fd26b421d187acc1450763597326e19a2974
ksarson <37591778+ksarson@users.noreply.github.com>
2023-09-29 17:50:13 -0400

    Completed exercise 11

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,4 @@
 const sumAll = function(num1, num2) {
-    console.log(typeof num2);
-    console.log(typeof num2);
     if(num1 < 0 || num2 < 0 || typeof(num1) !== "number" || typeof(num2) !== "number"){
         return 'ERROR';
     }
