577ffd44628707c556fd89f008360ef28f912dca
ksarson <37591778+ksarson@users.noreply.github.com>
2023-09-30 10:27:12 -0400

    Completed exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  return people.reduce((oldestPerson, currentPerson) => 
+  {
+    const oldestAge = getAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+    const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+    return oldestAge < currentAge ? currentPerson : oldestPerson;
+  });
+};
+  
+const getAge = function (birth, death) {
+  if (!death)
+  {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
 };
 
 // Do not edit below this line
